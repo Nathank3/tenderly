2bbf85c9e787c15b459adbbb7ff2f6c64312989b
Nathank3 <kimeunathan8127@gmail.com>
2022-05-23 11:56:12 +0300

    adding new files

---
+++ admin/sendmessages.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+
+
+ ?>
+<!DOCTYPE html>
+<!--<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <button> <a href="https://nomaxsms.me/nexmo.php"> SEND MESSAGES</a></button>
+  </body>
+</html>
---
+++ includes/brands-slider.php
@@ -0,0 +1,99 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Our Partners</h3>
+		<div class="logo-slider-inner">
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/kplc.png" src="assets/images/blank.gif" alt="">
+					</a>
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/kemsa.png" src="assets/images/blank.gif" alt="">
+					</a>
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/kena.png" src="assets/images/blank.gif" alt="">
+					</a>
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/versace.png" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/kra.png" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/klb.png" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/dell.jpg" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/samsung.jpg" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+				<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/forbes.jpg" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+
+
+
+				<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/sony.jpg" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/lg.jpg" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/lg.jpg" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img class="newicons" data-echo="brandsimage/lenovo.jpg" src="assets/images/blank.gif" alt="">
+					</a>
+				</div>
+
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+		</div><!-- /.logo-slider-inner -->
+
+</div><!-- /.logo-slider -->
+
+<style type="text/css">
+	.newicons
+	{
+		display: inline-block;
+		width: 133px;
+		height: 48px;
+
+	}
+</style>
